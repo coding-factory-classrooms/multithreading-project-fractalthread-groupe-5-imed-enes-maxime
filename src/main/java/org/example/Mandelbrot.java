53fae6d731e3032273d1d18f05202a9ff59492fa
ImedItescia <imededdine.fodeilla@edu.itescia.fr>
2021-01-06 17:03:17 +0100

    meantime elapsed

--- src/main/java/org/example/Mandelbrot.java
+++ src/main/java/org/example/Mandelbrot.java
@@ -7,9 +7,9 @@ import javax.imageio.ImageIO;
 
 public class Mandelbrot {
     public static void main(String[] args) throws Exception {
-        int width = 1920, height = 1080, max = 1000;
+        int width = 1000, height = 1000, max = 1000;
 
-        double xPos = 0.5,yPos = 0, zoom = 1;
+        double xPos = 0.35, yPos = 0.095, zoom = 0.009;
 
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         int black = 0;
@@ -18,24 +18,33 @@ public class Mandelbrot {
             colors[i] = Color.HSBtoRGB(i/256f, 1, i/(i+8f));
         }
 
-        for (int row = 0; row < height; row++) {
-            for (int col = 0; col < width; col++) {
-                double c_re = ((col - width/2)*zoom/width) + xPos;
-                double c_im = ((row - height/2)*zoom/width) + yPos;
-                double x = 0, y = 0;
-                double r2;
-                int iteration = 0;
-                while (x*x+y*y < 4 && iteration < max) {
-                    double x_new = x*x-y*y+c_re;
-                    y = 2*x*y+c_im;
-                    x = x_new;
-                    iteration++;
+        int i = 0;
+        long meanTime =0;
+        while(i < 10){
+            long start = System.currentTimeMillis();
+            for (int row = 0; row < height; row++) {
+                for (int col = 0; col < width; col++) {
+                    double c_re = ((col - width/2)*zoom/width) + xPos;
+                    double c_im = ((row - height/2)*zoom/width) + yPos;
+                    double x = 0, y = 0;
+                    int iteration = 0;
+                    while (x*x+y*y < 4 && iteration < max) {
+                        double x_new = x*x-y*y+c_re;
+                        y = 2*x*y+c_im;
+                        x = x_new;
+                        iteration++;
+                    }
+                    if (iteration < max) image.setRGB(col, row, colors[iteration]);
+                    else image.setRGB(col, row, black);
                 }
-                if (iteration < max) image.setRGB(col, row, colors[iteration]);
-                else image.setRGB(col, row, black);
             }
+            ImageIO.write(image, "png", new File("mandelbrot.png"));
+            i++;
+            long elapsed = System.currentTimeMillis() - start;
+            meanTime += elapsed/i;
         }
+        System.out.println(meanTime + " ms");
 
-        ImageIO.write(image, "png", new File("mandelbrot.png"));
+
     }
 }
