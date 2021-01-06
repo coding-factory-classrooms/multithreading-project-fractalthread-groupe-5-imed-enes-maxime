2d576c50f511dc2191d31e08ff3de9bfd19d7f5b
Maxime <maximeld19@gmail.com>
2021-01-06 16:32:24 +0100

    Ajout variables

--- src/main/java/org/example/Mandelbrot.java
+++ src/main/java/org/example/Mandelbrot.java
@@ -8,6 +8,9 @@ import javax.imageio.ImageIO;
 public class Mandelbrot {
     public static void main(String[] args) throws Exception {
         int width = 1920, height = 1080, max = 1000;
+
+        double xPos = 0.5,yPos = 0, zoom = 1;
+
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         int black = 0;
         int[] colors = new int[max];
@@ -17,8 +20,8 @@ public class Mandelbrot {
 
         for (int row = 0; row < height; row++) {
             for (int col = 0; col < width; col++) {
-                double c_re = (col - width/2)*4.0/width;
-                double c_im = (row - height/2)*4.0/width;
+                double c_re = ((col - width/2)*zoom/width) + xPos;
+                double c_im = ((row - height/2)*zoom/width) + yPos;
                 double x = 0, y = 0;
                 double r2;
                 int iteration = 0;
