cf1f00f1ae01a5e5e77ba1edb27a9ab1d90a389a
ImedItescia <imededdine.fodeilla@edu.itescia.fr>
2021-01-08 17:19:40 +0100

    colors

--- src/main/java/org/example/Mandelbrot.java
+++ src/main/java/org/example/Mandelbrot.java
@@ -19,7 +19,7 @@ public class Mandelbrot {
     private static int[] getColors(int max){
         int[] colors = new int[max];
         for (int i = 0; i<max; i++) {
-            if(i < max/33){
+            if(i <= 50){
                 colors[i] = new Color(255,255,255).getRGB();
             }else {
                 colors[i] = Color.HSBtoRGB(i/256f, 1, i/(i+8f));
