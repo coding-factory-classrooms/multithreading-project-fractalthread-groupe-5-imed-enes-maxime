604e8575851f9c25fc02a2eb1cda7eb45d2c5802
Maxime <maximeld19@gmail.com>
2021-01-12 15:23:30 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/main/java/org/example/Mandelbrot.java
+++ src/main/java/org/example/Mandelbrot.java
@@ -3,16 +3,18 @@ package org.example;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class Mandelbrot {
+public class Mandelbrot{
     private final int maxIteration;
     private final int[] colors;
+    private final int nbThread;
 
-    public Mandelbrot(int maxIteration){
+    public Mandelbrot(int maxIteration, int nbThread){
         this.maxIteration = maxIteration;
         this.colors = getColors(maxIteration);
+
+        this.nbThread = nbThread;
     }
 
     private static int[] getColors(int max){
@@ -27,12 +29,11 @@ public class Mandelbrot {
         return colors;
     }
 
-    public BufferedImage createFractal(int width , int height, double xPos, double yPos, double zoom){
+    public BufferedImage createFractal(int width , int height, double xPos, double yPos, double zoom, ExecutorService threadPool){
+
         long start = System.currentTimeMillis();
         Fractal fractal = new Fractal(width,height);
-        int nbThreadMax = 16;
-        int nbThread = nbThreadMax * 2;
-        ExecutorService threadPool = Executors.newFixedThreadPool(nbThreadMax);
+
 
         int chunkSizeX = width / nbThread;
         int chunkSizeY = height / nbThread;
@@ -52,7 +53,7 @@ public class Mandelbrot {
                 if(height - nextY < 0){
                     nextY = y + height - y;
                 }
-                CreateFractalTask task = new CreateFractalTask(width,height,colors,maxIteration,x,y,nextX,nextY,xPos,yPos,zoom, fractal);
+                MandelbrotFractalTask task = new MandelbrotFractalTask(width,height,colors,maxIteration,x,y,nextX,nextY,xPos,yPos,zoom, fractal);
 
                 threadPool.execute(task);
 
@@ -71,4 +72,23 @@ public class Mandelbrot {
         System.out.println("time create : " + (System.currentTimeMillis() - start) + " ms");
         return fractal.getImage();
     }
+
+    public static int getPixelIteration(int width, int height, double xPos, double yPos, double zoom, int col, int row,int maxIteration){
+
+        int widthHalf = width/2;
+        int heightHalf = height/2;
+
+        double c_re = ((col - widthHalf)*zoom/width) + xPos;
+        double c_im = ((row - heightHalf)*zoom/width) + yPos;
+        double x = 0, y = 0;
+        int iteration = 0;
+        while (x*x+y*y < 4 && iteration < maxIteration) {
+            double x_new = x*x-y*y+c_re;
+            y = 2*x*y+c_im;
+            x = x_new;
+            iteration++;
+        }
+        return iteration;
+    }
+
 }
--- src/main/java/org/example/MandelbrotFractalTask.java
+++ src/main/java/org/example/MandelbrotFractalTask.java
@@ -1,5 +1,5 @@
 package org.example;
-public class CreateFractalTask implements Runnable{
+public class MandelbrotFractalTask implements Runnable{
 
     private final int width;
     private final int height;
@@ -14,7 +14,7 @@ public class CreateFractalTask implements Runnable{
     private final double zoom;
     private final Fractal fractal;
 
-    public CreateFractalTask(int width, int height, int[] colors, int maxIteration, int startX, int startY, int endX, int endY, double xPos, double yPos, double zoom, Fractal fractal) {
+    public MandelbrotFractalTask(int width, int height, int[] colors, int maxIteration, int startX, int startY, int endX, int endY, double xPos, double yPos, double zoom, Fractal fractal) {
         this.width = width;
         this.height = height;
         this.colors = colors;
@@ -32,24 +32,17 @@ public class CreateFractalTask implements Runnable{
 
     @Override
     public void run() {
-
-        int widthHalf = width/2;
-        int heightHalf = height/2;
-
         for (int row = startY; row < endY; row++) {
             for (int col = startX; col < endX; col++) {
-                double c_re = ((col - widthHalf)*zoom/width) + xPos;
-                double c_im = ((row - heightHalf)*zoom/width) + yPos;
-                double x = 0, y = 0;
-                int iteration = 0;
-                while (x*x+y*y < 4 && iteration < maxIteration) {
-                    double x_new = x*x-y*y+c_re;
-                    y = 2*x*y+c_im;
-                    x = x_new;
-                    iteration++;
+
+                int iteration = Mandelbrot.getPixelIteration(width,height,xPos,yPos,zoom,col,row,maxIteration);
+
+                if (iteration < maxIteration){
+                    fractal.append(col, row, colors[iteration]);
                 }
-                if (iteration < maxIteration) fractal.append(col, row, colors[iteration]);
-                else fractal.append(col, row, 0);
+                else{
+                    fractal.append(col, row, 0);
+                }
             }
         }
     }
